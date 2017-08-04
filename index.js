a21d75ebc4a07077d08dd781813de093c52112a1
KUO <drjkuo@gmail.com>
2017-08-04 16:34:59 -0700

    res.send() in-place

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 var Twitter = require('twitter-node-client').Twitter;
 var fs = require('fs');
 var config = JSON.parse(fs.readFileSync('./data/twitter_config', encoding="ascii"));
+var express = require('express');
+var app = express();
+var path = require('path');
 
 var toBeSent = [];
 //Callback functions
+
+
+var twitter = new Twitter(config);
+	//Example calls
+
 var error = function (err, response, body) {
     console.log('ERROR [%s]', err);
 };
@@ -15,22 +23,18 @@ var success = function (data) {
       toBeSent.push(data.statuses[i].id_str);
       // toBeSent += (`Data #${i}` + data.statuses[i].text);
     }
-
+    toBeSent = JSON.stringify(toBeSent);
+    // res.send(toBeSent);
+    console.log(toBeSent);
     // console.log('Data [%s]', data);
 };
-
-var twitter = new Twitter(config);
-	//Example calls
-
 	// twitter.getUserTimeline({ screen_name: 'drjkuo', count: '1'}, error, success);
 // twitter.getUserTimeline({ screen_name: 'nytimes','result\_type':'popular', count: '3'}, error, success);
 // twitter.getSearch({'q':' movie -scary :) since:2013-12-27', 'count': 10, 'result\_type':'popular'}, error, success);
 
 
 ///////////////////////////
-var express = require('express');
-var app = express();
-var path = require('path');
+
 
 
 app.get('/', function (req, res) {
@@ -39,26 +43,32 @@ app.get('/', function (req, res) {
   // res.send(toBeSent);
 });
 
-// var cb0 = function (req, res, next) {
+// var cb0 = function (req, res, next, cb1) {
 //   console.log(req.query.twitterID);
 //   var q = "from:"+req.query.twitterID;
 //   twitter.getSearch({ 'q': q , 'result\_type':'popular', 'count': '10'}, error, success);
-//   next();
+//   console.log("FIRST!!!!!");
+//   console.log(toBeSent);
+//   cb1(req, res, next);
+// }
+//
+// var sent = function (req, res, next) {
+//   toBeSent = JSON.stringify(toBeSent);
+//   console.log("SECOND!!!!!");
+//   res.send(toBeSent);
 // }
 
-app.get('/hot', function (req, res, next) {
-  console.log(req.query.twitterID);
+app.get('/hot', function (req, res) {
   var q = "from:"+req.query.twitterID;
-  twitter.getSearch({ 'q': q , 'result\_type':'popular', 'count': '10'}, error, success);
-  console.log("FIRST!!!!!");
-  console.log(toBeSent);
-  next();
-}, function (req, res, next) {
-  // if (typeof(toBeSent) !== "string") {
-    toBeSent = JSON.stringify(toBeSent);
-    console.log("SECOND!!!!!");
-    res.send(toBeSent);
-  // }
+  twitter.getSearch({ 'q': q , 'result\_type':'popular', 'count': '10'}, error, function (data) {
+      toBeSent = [];
+      data = JSON.parse(data);
+      for (var i=0; i<data.statuses.length; i++) {
+        toBeSent.push(data.statuses[i].id_str);
+      }
+      toBeSent = JSON.stringify(toBeSent);
+      res.send(toBeSent);
+  });
 });
 
 app.listen(3000, function () {
