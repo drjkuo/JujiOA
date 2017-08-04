f402f53a661bc905b12f6a4dc78c420933418983
KUO <drjkuo@gmail.com>
2017-08-04 01:20:26 -0700

    add http server

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var Twitter = require('twitter-node-client').Twitter;
 var fs = require('fs');
 var config = JSON.parse(fs.readFileSync('./data/twitter_config', encoding="ascii"));
 
+var toBeSent = "";
 //Callback functions
 var error = function (err, response, body) {
     console.log('ERROR [%s]', err);
@@ -10,7 +11,9 @@ var success = function (data) {
     data = JSON.parse(data);
     for (var i=0; i<data.statuses.length; i++) {
       console.log(`Data #${i}`, data.statuses[i].text);
+      // toBeSent += (`Data #${i}` + data.statuses[i].text);
     }
+    toBeSent = data;
     // console.log('Data [%s]', data);
 };
 
@@ -21,3 +24,17 @@ var twitter = new Twitter(config);
 // twitter.getUserTimeline({ screen_name: 'nytimes','result\_type':'popular', count: '3'}, error, success);
 // twitter.getSearch({'q':' movie -scary :) since:2013-12-27', 'count': 10, 'result\_type':'popular'}, error, success);
 twitter.getSearch({ 'q':'from:nytimes' , 'result\_type':'popular', 'count': '3'}, error, success);
+
+///////////////////////////
+var express = require('express');
+var app = express();
+var path = require('path');
+
+
+app.get('/', function (req, res) {
+  res.send(toBeSent);
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
