55891ade63a47493082dfe34d8b3b868bf85a7d4
KUO <drjkuo@gmail.com>
2017-08-04 12:04:06 -0700

    /hot for ajax use

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var Twitter = require('twitter-node-client').Twitter;
 var fs = require('fs');
 var config = JSON.parse(fs.readFileSync('./data/twitter_config', encoding="ascii"));
 
-var toBeSent = "";
+var toBeSent = [];
 //Callback functions
 var error = function (err, response, body) {
     console.log('ERROR [%s]', err);
@@ -11,9 +11,10 @@ var success = function (data) {
     data = JSON.parse(data);
     for (var i=0; i<data.statuses.length; i++) {
       console.log(`Data #${i}`, data.statuses[i].text);
+      toBeSent.push(data.statuses[i].id_str);
       // toBeSent += (`Data #${i}` + data.statuses[i].text);
     }
-    toBeSent = data;
+
     // console.log('Data [%s]', data);
 };
 
@@ -23,7 +24,7 @@ var twitter = new Twitter(config);
 	// twitter.getUserTimeline({ screen_name: 'drjkuo', count: '1'}, error, success);
 // twitter.getUserTimeline({ screen_name: 'nytimes','result\_type':'popular', count: '3'}, error, success);
 // twitter.getSearch({'q':' movie -scary :) since:2013-12-27', 'count': 10, 'result\_type':'popular'}, error, success);
-twitter.getSearch({ 'q':'from:nytimes' , 'result\_type':'popular', 'count': '3'}, error, success);
+twitter.getSearch({ 'q':'from:nytimes' , 'result\_type':'popular', 'count': '10'}, error, success);
 
 ///////////////////////////
 var express = require('express');
@@ -34,6 +35,11 @@ var path = require('path');
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
   // res.send(toBeSent);
+});
+
+app.get('/hot', function (req, res) {
+  // res.sendFile(path.join(__dirname + '/index.html'));
+  res.send(toBeSent);
 });
 
 app.listen(3000, function () {
