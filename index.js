985f758c4cc28ff0fed69cb7317bb331fb890377
KUO <drjkuo@gmail.com>
2017-08-04 14:08:54 -0700

    need to pass cb function into /hot route

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var success = function (data) {
     toBeSent = [];
     data = JSON.parse(data);
     for (var i=0; i<data.statuses.length; i++) {
-      console.log(`Data #${i}`, data.statuses[i].text);
+      // console.log(`Data #${i}`, data.statuses[i].text);
       toBeSent.push(data.statuses[i].id_str);
       // toBeSent += (`Data #${i}` + data.statuses[i].text);
     }
@@ -25,7 +25,7 @@ var twitter = new Twitter(config);
 	// twitter.getUserTimeline({ screen_name: 'drjkuo', count: '1'}, error, success);
 // twitter.getUserTimeline({ screen_name: 'nytimes','result\_type':'popular', count: '3'}, error, success);
 // twitter.getSearch({'q':' movie -scary :) since:2013-12-27', 'count': 10, 'result\_type':'popular'}, error, success);
-twitter.getSearch({ 'q':'from:nytimes' , 'result\_type':'popular', 'count': '10'}, error, success);
+
 
 ///////////////////////////
 var express = require('express');
@@ -34,16 +34,31 @@ var path = require('path');
 
 
 app.get('/', function (req, res) {
+  toBeSent = [];
   res.sendFile(path.join(__dirname + '/index.html'));
   // res.send(toBeSent);
 });
 
-app.get('/hot', function (req, res) {
-  // res.sendFile(path.join(__dirname + '/index.html'));
-  if (typeof(toBeSent) !== "string") {
+// var cb0 = function (req, res, next) {
+//   console.log(req.query.twitterID);
+//   var q = "from:"+req.query.twitterID;
+//   twitter.getSearch({ 'q': q , 'result\_type':'popular', 'count': '10'}, error, success);
+//   next();
+// }
+
+app.get('/hot', function (req, res, next) {
+  console.log(req.query.twitterID);
+  var q = "from:"+req.query.twitterID;
+  twitter.getSearch({ 'q': q , 'result\_type':'popular', 'count': '10'}, error, success);
+  console.log("FIRST!!!!!");
+  console.log(toBeSent);
+  next();
+}, function (req, res, next) {
+  // if (typeof(toBeSent) !== "string") {
     toBeSent = JSON.stringify(toBeSent);
-  }
-  res.send(toBeSent);
+    console.log("SECOND!!!!!");
+    res.send(toBeSent);
+  // }
 });
 
 app.listen(3000, function () {
