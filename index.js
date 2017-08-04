123489b68fcafaba30602f7a017dbda8b4aff6e6
KUO <drjkuo@gmail.com>
2017-08-03 23:48:10 -0700

    print text of each tweet

--- index.js
+++ index.js
@@ -7,10 +7,16 @@ var error = function (err, response, body) {
     console.log('ERROR [%s]', err);
 };
 var success = function (data) {
-    console.log('Data [%s]', data);
+    data = JSON.parse(data);
+    for (var i=0; i<data.length; i++) {
+      console.log(`Data #${i}`, data[i].text);
+    }
+    // console.log('Data [%s]', data);
 };
 
 var twitter = new Twitter(config);
 	//Example calls
 
-	twitter.getUserTimeline({ screen_name: 'drjkuo', count: '1'}, error, success);
+	// twitter.getUserTimeline({ screen_name: 'drjkuo', count: '1'}, error, success);
+	twitter.getUserTimeline({ screen_name: 'medium','result\_type':'popular', count: '5'}, error, success);
+// twitter.getSearch({'q':' movie -scary :) since:2013-12-27', 'count': 10, 'result\_type':'popular'}, error, success);
